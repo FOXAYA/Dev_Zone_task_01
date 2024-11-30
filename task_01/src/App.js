15f3f2989eb0fbe0eb75a6c484c9f870de27c9b5
FOXAYA <omniasaged111@gmail.com>
2024-11-30 23:31:42 +0200

    updated

--- task_01/src/App.js
+++ task_01/src/App.js
@@ -1,13 +1,17 @@
 import { Component } from 'react';
 import './App.css';
 import Buttons from './compentents/Buttons';
+import Content  from './compentents/Content';
 
 
 class App extends Component{
   
   render(){
     return (
+      <>
       <Buttons />
+      <Content />
+      </>
     )
   }
 }
